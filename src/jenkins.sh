19ef85f8120648b97dfeb3eb0ba6a8617be38ce9
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-06-03 20:21:00 +1200

    Adding the java arguments
    references #2

--- src/jenkins.sh
+++ src/jenkins.sh
@@ -6,6 +6,15 @@ JAVA_G1_GC_ARGS="-XX:+UseG1GC -XX:+ExplicitGCInvokesConcurrent -XX:+ParallelRefP
 JAVA_HEAP_ARGS="-Xms512m -Xmx1024m"
 JAVA_AWT_ARGS="-Djava.awt.headless=true"
 
+# Settings (from here: https://wiki.jenkins-ci.org/display/JENKINS/Features+controlled+by+system+properties)
+# -hudson.model.UpdateCenter.never -> never download new jenkins versions
+# -hudson.model.DownloadService.never -> never download new plugin information
+# -jenkins.model.Jenkins.slaveAgentPort -> set the port for the slaves to connect to jenkins
+# -jenkins.model.Jenkins.slaveAgentPortEnforce -> enforce the slave connection port. Cannot change it from the UI.
+# -jenkins.CLI.disabled -> Disable the CLI through JNLP and HTTP
+# -jenkins.install.runSetupWizard -> Skip the install wizzard
+JENKINS_JAVA_ARGS="-Dhudson.model.UpdateCenter.never=true -Dhudson.model.DownloadService.never=true -Djenkins.model.Jenkins.slaveAgentPort=$JENKINS_SLAVE_AGENT_PORT -Djenkins.model.Jenkins.slaveAgentPortEnforce=true -Djenkins.CLI.disabled=true -Djenkins.install.runSetupWizard=false"
+
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec /sbin/setuser jenkins java $JAVA_SERVER_ARGS $JAVA_G1_GC_ARGS $JAVA_HEAP_ARGS $JAVA_AWT_ARGS -jar /etc/jenkins/jenkins.war --httpPort=8080 --prefix=/jenkins 2>&1 | logger
+exec /sbin/setuser jenkins java $JAVA_SERVER_ARGS $JAVA_G1_GC_ARGS $JAVA_HEAP_ARGS $JAVA_AWT_ARGS $JENKINS_JAVA_ARGS -jar /usr/local/jenkins/jenkins.war --httpPort=8080 2>&1 | logger
