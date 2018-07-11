29634687f1ff3018d7ec695f87d678f298151e36
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-07-12 08:20:15 +1200

    Updating the plugins with additional plugins that are required

--- src/Get-PluginVersions.ps1
+++ src/Get-PluginVersions.ps1
@@ -13,30 +13,45 @@ $commonParameterSwitches =
 $expectedPluginsPath = Join-Path $PSScriptRoot 'plugins.xml'
 $expectedPlugins = [xml](Get-Content -Path $expectedPluginsPath | Out-String)
 
+$jenkinsPluginUrl = "https://updates.jenkins.io/$($jenkinsVersion)/update-center.actual.json"
+Write-Verbose "Getting plugin information from $jenkinsPluginUrl ..."
 $response = Invoke-WebRequest `
-    -Uri "https://updates.jenkins.io/$($jenkinsVersion)/update-center.actual.json" `
+    -Uri $jenkinsPluginUrl `
     -UseBasicParsing `
     @commonParameterSwitches
 $json = ConvertFrom-Json $response.Content
 $plugins = $json.plugins
 
 $rubyIncludeFile = Join-Path (Join-Path (Join-Path (Join-Path $PSScriptRoot 'cookbooks') 'resource_build_controller') 'attributes' ) 'jenkins_plugin_versions.rb'
+Write-Verbose "Found ruby fille at: $rubyIncludeFile"
 
 $fileContent = '# frozen_string_literal: true' + [System.Environment]::NewLine
 $fileContent += '' + [System.Environment]::NewLine
 $fileContent += "default['jenkins']['plugins'] = {" + [System.Environment]::NewLine
 
+Write-Verbose "Getting versions for plugins ..."
 $childItems = $expectedPlugins.DocumentElement.ChildNodes
 for ($i = 0; $i -lt $childItems.Count; $i++)
 {
     $element = $childItems[$i]
-    $plugin = $plugins."$($element.InnerText)"
-    if (($plugin.name -eq '') -or ($plugin.name -eq $null))
+    $pluginName = $element.Attributes["id"].Value
+    $pluginVersion = $element.Attributes["version"].Value
+
+    if ($pluginVersion -eq $null)
     {
-        continue
+        Write-Verbose "Finding version for $pluginName ..."
+        $plugin = $plugins."$($pluginName)"
+        if (($plugin -eq $null) -or ($plugin.name -eq '') -or ($plugin.name -eq $null))
+        {
+            continue
+        }
+
+        $pluginVersion = $plugin.version
     }
 
-    $text = "  '$($plugin.name)' => '$($plugin.version)'"
+    Write-Verbose "Version for $pluginName is $pluginVersion"
+
+    $text = "  '$($pluginName)' => '$($pluginVersion)'"
     if (($i + 1) -ne $childItems.Count)
     {
         $text += ','
@@ -49,3 +64,5 @@ $fileContent += '}'
 
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 [System.IO.File]::WriteAllLines($rubyIncludeFile, $fileContent, $Utf8NoBomEncoding)
+
+Write-Output "Plugin list processed. Include file is at: $($rubyIncludeFile)"
